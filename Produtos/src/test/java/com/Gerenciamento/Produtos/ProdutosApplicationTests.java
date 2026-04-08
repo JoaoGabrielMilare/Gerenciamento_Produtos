9e4aca84a020aea7892597547c96c20aec913dbf
unknown <you@example.com>
2026-04-07 21:04:44 -0300

    Iniciando projeto

---
+++ Produtos/src/test/java/com/Gerenciamento/Produtos/ProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciamento.Produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
